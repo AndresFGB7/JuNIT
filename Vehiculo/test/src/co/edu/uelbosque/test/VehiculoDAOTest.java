e99d5eb39b6a333cd3a597725cc865939525ecf3
AndresFGB7 <agalvisb@unbosque.edu.co>
2020-11-07 02:29:14 -0500

    Commit

--- Vehiculo/test/src/co/edu/uelbosque/test/VehiculoDAOTest.java
+++ Vehiculo/test/src/co/edu/uelbosque/test/VehiculoDAOTest.java
@@ -8,6 +8,12 @@ import co.edu.uelbosque.modelo.persistence.Archivo;
 import co.edu.uelbosque.modelo.persistence.VehiculoDAO;
 import junit.framework.TestCase;
 
+/**
+ * En esta clase realizamos un test de la clase VehiculoDAO<br>
+ * <b>pre</b> Debe Terminar el nombre en Test para el correcto
+ * funcionamiento<br>
+ * <b>post</b> Podremos saber que funciona la clase
+ */
 public class VehiculoDAOTest extends TestCase {
 
 	VehiculoDAO vehiculo;
@@ -21,6 +27,11 @@ public class VehiculoDAOTest extends TestCase {
 	Vehiculo vehiculo_5;
 	String f = "dataTest/VehiculoTest.dat";
 
+	/**
+	 * Este metodo prepara el escenario para las pruebas que se realizarán
+	 * <b>pre</b> Deben existir unos componentes<br>
+	 * <b>post</b> Se prepara el escenario
+	 */
 	public void setupEscenario() {
 		file.delete();
 		try {
@@ -51,35 +62,73 @@ public class VehiculoDAOTest extends TestCase {
 		System.out.println(lista.size());
 	}
 
+	/**
+	 * Se mira que funcione el metodo agregarVehiculo
+	 * <b>pre</b> Debe Existir el metodo<br>
+	 * <b>post</b> Podremos saber que funciona el metodo
+	 */
 	public void testAgregarVehiculo() {
 		setupEscenario();
-		assertTrue("se debio agregar el vehiculo",vehiculo.agregarVehiculo(vehiculo_1.getModelo(), vehiculo_1.getMarca(), vehiculo_1.getPlaca(), vehiculo_1.getnPuertas(), vehiculo_1.getnAsientos(), vehiculo_1.getTipoVehiculo(), vehiculo_1.getEstado(), f));
-		assertFalse("no se ddeberia agregar el vehiculo con placa repetida",vehiculo.agregarVehiculo(vehiculo_5.getModelo(), vehiculo_5.getMarca(), vehiculo_5.getPlaca(), vehiculo_5.getnPuertas(), vehiculo_5.getnAsientos(), vehiculo_5.getTipoVehiculo(), vehiculo_5.getEstado(), f));;	
+		assertTrue("se debio agregar el vehiculo",
+				vehiculo.agregarVehiculo(vehiculo_1.getModelo(), vehiculo_1.getMarca(), vehiculo_1.getPlaca(),
+						vehiculo_1.getnPuertas(), vehiculo_1.getnAsientos(), vehiculo_1.getTipoVehiculo(),
+						vehiculo_1.getEstado(), f));
+		assertFalse("no se ddeberia agregar el vehiculo con placa repetida",
+				vehiculo.agregarVehiculo(vehiculo_5.getModelo(), vehiculo_5.getMarca(), vehiculo_5.getPlaca(),
+						vehiculo_5.getnPuertas(), vehiculo_5.getnAsientos(), vehiculo_5.getTipoVehiculo(),
+						vehiculo_5.getEstado(), f));
+		;
 	}
-
+	/**
+	 * Se mira que funcione el metodo agregarVehiculo
+	 * <b>pre</b> Debe Existir el metodo<br>
+	 * <b>post</b> Podremos saber que funciona el metodo
+	 */
+	
 	public void testMostrarVehiculo() {
 		setupEscenario();
-		assertNull("No se encontro el vehiculo",vehiculo.mostrarVehiculo("444-GAE", lista));
-		assertNotNull("Se encontro el vehiculo",vehiculo.mostrarVehiculo(vehiculo_1.getPlaca(), lista));
+		assertNull("No se encontro el vehiculo", vehiculo.mostrarVehiculo("444-GAE", lista));
+		assertNotNull("Se encontro el vehiculo", vehiculo.mostrarVehiculo(vehiculo_1.getPlaca(), lista));
 	}
+	/**
+	 * Se mira que funcione el metodo mostrarLista
+	 * <b>pre</b> Debe Existir el metodo<br>
+	 * <b>post</b> Podremos saber que funciona el metodo
+	 */
 
 	public void testMostrarLista() {
 		setupEscenario();
-		assertNotNull("Se devuelve la lista de vehiculos",vehiculo.mostrarLista(lista));
+		assertNotNull("Se devuelve la lista de vehiculos", vehiculo.mostrarLista(lista));
 	}
+	/**
+	 * Se mira que funcione el metodo eliminarVehiculo
+	 * <b>pre</b> Debe Existir el metodo<br>
+	 * <b>post</b> Podremos saber que funciona el metodo
+	 */
 
 	public void testEliminarVehiculo() {
 		setupEscenario();
-		assertTrue("Se debio eliminar el vehiculo",vehiculo.eliminarVehiculo(vehiculo_2.getPlaca(), lista, file));
-		assertFalse("No se debio eliminar el vehiculo",vehiculo.eliminarVehiculo("327-HJS", lista, file));
+		assertTrue("Se debio eliminar el vehiculo", vehiculo.eliminarVehiculo(vehiculo_2.getPlaca(), lista, file));
+		assertFalse("No se debio eliminar el vehiculo", vehiculo.eliminarVehiculo("327-HJS", lista, file));
 	}
+	/**
+	 * Se mira que funcione el metodo mostrarInformacion
+	 * <b>pre</b> Debe Existir el metodo<br>
+	 * <b>post</b> Podremos saber que funciona el metodo
+	 */
 
 	public void testMostrarInformacion() {
 		setupEscenario();
-		assertNotNull("Debio retornar la informacion del vehiculo",vehiculo.mostrarInfoString(vehiculo_1.getPlaca(), lista));
+		assertNotNull("Debio retornar la informacion del vehiculo",
+				vehiculo.mostrarInfoString(vehiculo_1.getPlaca(), lista));
 		assertNull("No debio retornar la informacion del vehiculo", vehiculo.mostrarInfoString("345-jha", lista));
 
 	}
+	/**
+	 * Se mira que funcione el metodo venderVehiculo
+	 * <b>pre</b> Debe Existir el metodo<br>
+	 * <b>post</b> Podremos saber que funciona el metodo
+	 */
 
 	public void testeVenderVehiculo() {
 		setupEscenario();
@@ -87,15 +136,28 @@ public class VehiculoDAOTest extends TestCase {
 		assertNotNull("Se vendio el vehiculo", vehiculo.venderVehiculo(vehiculo_1.getPlaca(), lista));
 
 	}
+	/**
+	 * Se mira que funcione el metodo estadoVehiculo
+	 * <b>pre</b> Debe Existir el metodo<br>
+	 * <b>post</b> Podremos saber que funciona el metodo
+	 */
+
 	public void testEstadoVehiculo() {
 		setupEscenario();
 		assertNull("No se encontro el vehiculo buscado", vehiculo.estadoVehiculo("126-gdh", lista));
 		assertNotNull("Se muestra el estado del vehiculo", vehiculo.estadoVehiculo(vehiculo_1.getPlaca(), lista));
 
-	}public void testCompararVehiculopor() {
+	}
+	/**
+	 * Se mira que funcione el metodo compararVehiculopor
+	 * <b>pre</b> Debe Existir el metodo<br>
+	 * <b>post</b> Podremos saber que funciona el metodo
+	 */
+
+	public void testCompararVehiculopor() {
 		setupEscenario();
-		assertNotNull("Se compararon los vehiculos", vehiculo.mostrarInfoPor(vehiculo_1.getPlaca(),"Modelo", lista));
-		assertNull("Se compararon los vehiculos", vehiculo.mostrarInfoPor("aa","Modelo", lista));
+		assertNotNull("Se compararon los vehiculos", vehiculo.mostrarInfoPor(vehiculo_1.getPlaca(), "Modelo", lista));
+		assertNull("Se compararon los vehiculos", vehiculo.mostrarInfoPor("aa", "Modelo", lista));
 	}
 
 }
